4b2ce6a0bcce2a2baf62890045e7372cd7dcaf72
DaKittengamer <kittengamer002@gmail.com>
2025-10-21 00:26:52 +0000

    website bug fix

--- Website/scripts/main.js
+++ Website/scripts/main.js
@@ -1,4 +1,13 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector("h1");
-// Update the text content of the <h1>
-myHeading.textContent = "";
+const listItems = document.querySelectorAll("li");
+
+function toggleDone(e) {
+  if (!e.target.className) {
+    e.target.className = "done";
+  } else {
+    e.target.className = "";
+  }
+}
+
+listItems.forEach((item) => {
+  item.addEventListener("click", toggleDone);
+});
